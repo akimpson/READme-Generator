ce43df8a9f8e26336804b6fe35592d073866d7c3
Aisha Kimpson <aishakimpson@Aishas-MacBook-Air.local>
2022-04-03 11:10:58 -0400

    created an array of questions

--- index.js
+++ index.js
@@ -7,7 +7,53 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: "input",
+        message: "What is the title of the project?",
+        name: "Title"
+    }, {
+        type: "input",
+        message: "What is the project about? Give a detailed description",
+        name: "Description"
+    }, {
+        type: "input",
+        message: "Table of Contents",
+        name: "Table of Contents"    
+    }, {
+        type: "input",
+        message: "What does the user need to install to run this app ",
+        name: "Installation"    
+    }, {
+        type: "input",
+        message: "How is the app used? Give instructions",
+        name: "Usage"
+    }, {
+        type: "input",
+        message: "What license is being used? (i.e... MIT)",
+        name: "License"   
+    }, {
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "Contributing"   
+    }, {
+        type: "input",
+        message: "What commands are needed to test this app?",
+        name: "Tests"  
+    }, {
+        type: "input",
+        message: "Contact information for inquiries",
+        name: "Questions"
+    }, {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "Username"               
+    }, {
+        type: "input",
+        message: "What is your email address?",
+        name: "Email"     
+    },
+
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
